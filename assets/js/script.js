0d44702cb262f7efd9a103ea5f4cec37f65299d4
Barox Phan <pqb.barox@gmail.com>
2021-12-11 16:15:26 +0700

    day 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,40 @@
 $(document).ready(function () {
-  const header = $("#header:not(#header.not-fixed");
+  let header = $("#header");
+  const navbarBtn = $(".navbar__btn");
   const headerWrapper = header.children(".wrapper");
+  const navbar = $(".navbar");
+  const overlay = $("#header .wrapper .overlay");
+
+  function showNavbar() {
+    navbar.css("transform", "translateX(0px)");
+    overlay.css("display", "block");
+  }
+  function hideNavbar() {
+    navbar.css("transform", "translateX(100%)");
+    overlay.css("display", "none");
+  }
 
   $(document).scroll(function (e) {
     const scrollY = window.scrollY;
-    if (scrollY > 150) {
+    if (scrollY > 100) {
       header.css("background-color", "#1c1c1c");
       headerWrapper.css({ "padding-top": "10px", "padding-bottom": "10px" });
+
+      if (header.is(".not-fixed")) {
+        header.css("position", "fixed");
+      }
     } else {
-      header.css("background-color", "transparent");
+      if (!header.is(".not-fixed")) {
+        header.css("background-color", "transparent");
+      } else {
+        header.css("position", "relative");
+      }
+
       headerWrapper.css({ "padding-top": "42px", "padding-bottom": "30px" });
     }
   });
+
+  navbarBtn.click(showNavbar);
+
+  overlay.click(hideNavbar);
 });
